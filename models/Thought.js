1c3d0dc10c6809e34002f45d6e5498333fc400ce
Sabrina Hanson <sabhanson7@gmail.com>
2022-02-25 18:27:59 -0800

    fixed errors in Thought.js, needed to change order of the schemas

--- models/Thought.js
+++ models/Thought.js
@@ -1,32 +1,6 @@
-const { Schema, models } = require('mongoose');
+const { Schema, model } = require('mongoose');
 const moment = require('moment');
 
-const thoughtSchema = new Schema (
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            minlength: 1,
-            maxlength: 280
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: (getTime)=> {moment(getTime).format('MMMM Do YYYY, h:mm:ss a')}
-        },
-        username: {
-            type: String,
-            required: true
-        },
-        reactions: [reactionSchema] //array of the reactions that belong to this thought
-    },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true
-        }
-    }
-)
 
 const reactionSchema = new Schema(
     {
@@ -55,6 +29,34 @@ const reactionSchema = new Schema(
         }
     }
 )
+
+const thoughtSchema = new Schema (
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (getTime)=> {moment(getTime).format('MMMM Do YYYY, h:mm:ss a')}
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema] //array of the reactions that belong to this thought
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        }
+    }
+)
+
 
 thoughtSchema.virtual('reactionCount').get(function() {
     return this.reactions.length;
