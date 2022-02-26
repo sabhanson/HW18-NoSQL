6c6501fb7bcfcbdd5182a385e4a8982b9c550ee0
Sabrina Hanson <sabhanson7@gmail.com>
2022-02-25 18:36:32 -0800

    user routes: put new user, get all users, and get single user by id are working

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -13,10 +13,37 @@ router.get('/', (req,res) =>{
     })
 })
 
-
 // get single user by id
+router.get('/:userId', (req,res) => {
+    User.findOne(
+        {_id: req.params.userId}
+    )
+    .populate('thoughts')
+    .populate('friends')
+    .select ('-__v')
+    .then((user) => {
+        if (!user) {
+            res.status(404).json({ message: "oops, user does not exist"})
+            return;
+        }
+        res.status(200).json(user)
+    })
+    .catch((err)=> {
+        console.error({message: err});
+        return res.status(500).json(err);
+    })
+})
+
 
 // create a new user
+router.post('/', (req,res) => {
+    User.create(req.body)
+    .then((users)=> res.status(200).json(users))
+    .catch((err)=> {
+        console.error({message: err});
+        return res.status(500).json(err);
+    })
+})
 
 // update an existing user
 
