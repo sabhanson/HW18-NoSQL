b460f5883956c29f97c10ed5d96be865e26b770e
Sabrina Hanson <sabhanson7@gmail.com>
2022-02-25 18:07:39 -0800

    psuedocode routes files

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,21 @@
+const { Thought } = require('../../models')
+
+const router = require('express').Router();
+
+// get all thoughts
+
+// get single thought by id
+
+// create a new thought
+
+// update an existing thought
+
+// delete an existing thought
+
+// add a new reaction
+
+// delete an existing reaction
+
+
+
+module.exports = router;
