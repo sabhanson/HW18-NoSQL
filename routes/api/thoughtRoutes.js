e64327d1038a806b7f0ba6ec04a07290acaa0ffe
Sabrina Hanson <sabhanson7@gmail.com>
2022-02-25 19:10:44 -0800

    added delete and updatet thought routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,25 +1,81 @@
-const { Thought, User } = require('../../models')
+const { Thought, User } = require("../../models");
 
-const router = require('express').Router();
+const router = require("express").Router();
 
 // get all thoughts
-router.get('/', (req,res) =>{
-    Thought.find()
-    .populate('reactions')
-    .then((thoughts)=> res.status(200).json(thoughts))
-    .catch((err)=> {
-        console.error({message: err});
-        return res.status(500).json(err);
-    })
-})
+router.get("/", (req, res) => {
+  Thought.find()
+    .populate("reactions")
+    .then((thoughts) => res.status(200).json(thoughts))
+    .catch((err) => {
+      console.error({ message: err });
+      return res.status(500).json(err);
+    });
+});
 
 // get single thought by id
-router.get('/:thoughtId', (req,res) => {
-    Thought.findOne(
-        {_id: req.params.thoughtId}
-    )
-    .populate('reactions')
-    .select ('-__v')
+router.get("/:thoughtId", (req, res) => {
+  Thought.findOne({ _id: req.params.thoughtId })
+    .populate("reactions")
+    .select("-__v")
+    .then((thought) => {
+      if (!thought) {
+        res.status(404).json({ message: "oops, thought does not exist" });
+        return;
+      }
+      res.status(200).json(thought);
+    })
+    .catch((err) => {
+      console.error({ message: err });
+      return res.status(500).json(err);
+    });
+});
+
+// create a new thought
+router.post("/", (req, res) => {
+  Thought.create(req.body)
+    .then((thought) => {
+      return User.findOneAndUpdate(
+        { username: req.body.username },
+        { $push: { thoughts: thought._id } },
+        { runValidators: true, new: true }
+      );
+    })
+    .then((newThought) => {
+      if (!newThought) {
+        res.status(404).json({ message: "oops, something went wrong" });
+        return;
+      }
+      res.status(200).json(newThought);
+    })
+    .catch((err) => {
+      console.error({ message: err });
+      return res.status(500).json(err);
+    });
+});
+
+// update an existing thought
+router.put("/:thoughtId", (req, res) => {
+  Thought.findOneAndUpdate({ _id: req.params.thoughtId }, req.body, {
+    runValidators: true,
+    new: true,
+  })
+    .then((thought) => {
+      if (!thought) {
+        res.status(404).json({ message: "oops, thought does not exist" });
+        return;
+      }
+      res.status(200).json(thought);
+    })
+    .catch((err) => {
+      console.error({ message: err });
+      return res.status(500).json(err);
+    });
+});
+
+// delete an existing thought
+router.delete('/:thoughtId', (req,res) => {
+    Thought.findOneAndDelete({_id: req.params.thoughtId})
     .then((thought) => {
         if (!thought) {
             res.status(404).json({ message: "oops, thought does not exist"})
@@ -33,34 +89,8 @@ router.get('/:thoughtId', (req,res) => {
     })
 })
 
-// create a new thought
-router.post('/', (req,res) => {
-    Thought.create(req.body)
-    .then((thought) => {
-        return User.findOneAndUpdate({username:req.body.username},{$push: {thoughts: thought._id}},{runValidators: true, new: true})
-    })
-    .then((newThought)=>{
-        if (!newThought) {
-            res.status(404).json({ message: "oops, something went wrong"})
-            return;
-        }
-        res.status(200).json(newThought)
-    })
-    .catch((err)=> {
-        console.error({message: err});
-        return res.status(500).json(err);
-    })
-})
-
-
-// update an existing thought
-
-// delete an existing thought
-
 // add a new reaction
 
 // delete an existing reaction
-
-
 
 module.exports = router;
