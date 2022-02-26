388355ca5104600eef698c21096b0fdb7f88fc8a
Sabrina Hanson <sabhanson7@gmail.com>
2022-02-25 18:54:12 -0800

    thought routes: post new thought, get all thoughts, and get single thought by id are done

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,12 +1,57 @@
-const { Thought } = require('../../models')
+const { Thought, User } = require('../../models')
 
 const router = require('express').Router();
 
 // get all thoughts
+router.get('/', (req,res) =>{
+    Thought.find()
+    .populate('reactions')
+    .then((thoughts)=> res.status(200).json(thoughts))
+    .catch((err)=> {
+        console.error({message: err});
+        return res.status(500).json(err);
+    })
+})
 
 // get single thought by id
+router.get('/:thoughtId', (req,res) => {
+    Thought.findOne(
+        {_id: req.params.thoughtId}
+    )
+    .populate('reactions')
+    .select ('-__v')
+    .then((thought) => {
+        if (!thought) {
+            res.status(404).json({ message: "oops, thought does not exist"})
+            return;
+        }
+        res.status(200).json(thought)
+    })
+    .catch((err)=> {
+        console.error({message: err});
+        return res.status(500).json(err);
+    })
+})
 
 // create a new thought
+router.post('/', (req,res) => {
+    Thought.create(req.body)
+    .then((thought) => {
+        return User.findOneAndUpdate({username:req.body.username},{$push: {thoughts: thought._id}},{runValidators: true, new: true})
+    })
+    .then((newThought)=>{
+        if (!newThought) {
+            res.status(404).json({ message: "oops, something went wrong"})
+            return;
+        }
+        res.status(200).json(newThought)
+    })
+    .catch((err)=> {
+        console.error({message: err});
+        return res.status(500).json(err);
+    })
+})
+
 
 // update an existing thought
 
