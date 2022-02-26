ac28f2f929de7dd4afc15839b682c46a0f6f13ee
Sabrina Hanson <sabhanson7@gmail.com>
2022-02-25 18:08:04 -0800

    psuedocode routes files, forgot to save D:

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,21 @@
+const { User } = require('../../models')
+
+const router = require('express').Router();
+
+// get all users
+
+// get single user by id
+
+// create a new user
+
+// update an existing user
+
+// delete an existing user
+
+// add a new friend
+
+// delete an existing friend
+
+
+
+module.exports = router;
